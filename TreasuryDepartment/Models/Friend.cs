7e9d34a53754f1d56ad1f2b42d6465ba2a30e126
dmitry <shemeldima2@yandex.ru>
2019-07-28 20:06:16 +0000

    Remove exceeds enities

--- TreasuryDepartment/Models/Friend.cs
+++ TreasuryDepartment/Models/Friend.cs
@@ -3,9 +3,7 @@ namespace TreasuryDepartment.Models
 	public class Friend
 	{
 		public long User1Id { get; set; }
-		public User User1 { get; set; }
 		public long User2Id { get; set; }
-		public User User2 { get; set; }
 
 		public Friend(long user1Id, long user2Id)
 		{
